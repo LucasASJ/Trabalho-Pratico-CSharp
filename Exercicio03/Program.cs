25cf25bd5e03e439478cc75ef4d0a4fd315408cb
LucasASJ <lucassjarek@gmail.com>
2025-05-01 15:57:41 -0300

    Exercicio03: Cálculo de Fatorial com while

--- Exercicio03/Program.cs
+++ Exercicio03/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Bem-vindo ao SuperMehaHiper cálculo de fatorial 3000!");
+        Console.Write("Informe um número inteiro e positivo: ");
+        int numero = int.Parse(Console.ReadLine());
+
+        if (numero < 0)
+        {
+            Console.WriteLine("Por favor, informe um número inteiro positivo.");
+        }
+        else
+        {
+            int fatorial = 1;
+            int i = 1;
+
+            while (i <= numero)
+            {
+                fatorial *= i;
+                i++;
+            }
+
+            Console.WriteLine($"O fatorial de {numero} é {fatorial}.");
+        }
+    }
+}
