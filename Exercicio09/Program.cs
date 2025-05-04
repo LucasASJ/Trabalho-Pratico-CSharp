808eade229b3659da37ff26403d25b200c542bc0
viniciusscss <viniciusdacostasilvasilva@gmail.com>
2025-05-04 18:32:56 -0300

    Update Program.cs

--- Exercicio09/Program.cs
+++ Exercicio09/Program.cs
@@ -1,2 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Random random = new Random();
+        int numeroSecreto = random.Next(1, 101); 
+        int tentativas = 0;
+        int palpite;
+
+        Console.WriteLine("Bem-vindo ao Jogo da Adivinhação!");
+        Console.WriteLine("Tente adivinhar o número entre 1 e 100.");
+
+        do
+        {
+            Console.Write("Digite seu palpite: ");
+            
+            while (!int.TryParse(Console.ReadLine(), out palpite));
+            {
+                Console.Write("Por favor, digite um número válido: ");
+            }
+
+            tentativas++;
+            
+            if (palpite < numeroSecreto)
+            {
+                Console.WriteLine("O número secreto é MAIOR!");
+            }
+            else if (palpite > numeroSecreto)
+            {
+                Console.WriteLine("O número secreto é MENOR!");
+            }
+
+        } while (palpite != numeroSecreto);
+
+        Console.WriteLine($"\nParabéns! :) Você acertou em ({tentativas}) tentativas!");
+    }
+}
